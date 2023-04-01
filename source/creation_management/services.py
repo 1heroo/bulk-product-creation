7eccc975663f6798af1ec57115527dc30cc524c3
Adil Shamuratov <iswearican_1@outlook.com>
2023-04-01 18:36:27 +0600

    clear

--- source/creation_management/services.py
+++ source/creation_management/services.py
@@ -27,9 +27,8 @@ class CreationServices:
                     + '_' + \
                     str(datetime.date.today()) + '.xlsx'
 
-        output_products_df.drop_duplicates(subset=['Артикул продавца'])
+        output_products_df = output_products_df.drop_duplicates(subset=['Артикул продавца'])
         output_products_df.to_excel(products_filename, index=False)
-
 
         excluded_df = self.creation_utils.excluded_df(
             initial_df=df, products_df=products_df, article_column=article_column)
